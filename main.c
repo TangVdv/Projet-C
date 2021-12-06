27641512cabc18a778f236bb8cd03640edd1da0c
TangVdv <tangrider77120@gmail.com>
2021-12-06 15:36:22 +0100

    Connect glade file to C

--- main.c
+++ main.c
@@ -1,15 +1,40 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 
+GtkApplication *app;
+GtkWidget *window;
+GtkBuilder *builder;
+GtkWidget *fixed1;
+GtkWidget *button1;
+GtkWidget *label1;
 
-int main(int argc, char **argv) {
-    GtkWidget *fenetre_principale = NULL;
-    GtkBuilder *builder = NULL;
-    GError *error = NULL;
-    gchar *filename = NULL;
-    /* Initialisation de la librairie Gtk. */
+
+int main(int argc, char **argv){
+
     gtk_init(&argc, &argv);
 
-    printf("Hello, World!\n");
+    builder = gtk_builder_new_from_file("Test.glade");
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+
+    app = gtk_application_new ("test.gtk.calendar", G_APPLICATION_FLAGS_NONE);
+
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    fixed1 = GTK_WIDGET(gtk_builder_get_object(builder, "fixed1"));
+    button1 = GTK_WIDGET(gtk_builder_get_object(builder, "button1"));
+    label1 = GTK_WIDGET(gtk_builder_get_object(builder, "label1"));
+
+
+    gtk_builder_connect_signals(builder, NULL);
+
+    gtk_widget_show(window);
+
+    gtk_main();
+
     return 0;
+}
+
+void on_button1_clicked (GtkButton *b){
+    gtk_label_set_text(GTK_LABEL(label1), (const gchar*) "Hello World");
 }
