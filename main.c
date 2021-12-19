f205a3dab6b3beb0b2c0105c0663b9cf49d9cb7e
TangVdv <tangrider77120@gmail.com>
2021-12-19 11:13:14 +0100

    système import/export done.

--- main.c
+++ main.c
@@ -9,31 +9,55 @@ GtkBuilder *builder;
 GtkWidget *button;
 sqlite3 *db;
 sqlite3_stmt *res;
+FILE *save_file;
+char *err_msg = 0;
+int rc;
 
-int callback(void *NotUsed, int rowCount, char **rowValue, char **rowName) {
-    char buffer[100];
-    char *num;
-    int i, j;
-    for (j = 1; j<3; j++) {
-        sprintf(num, "%d", j);
-        strcat(strcpy(buffer, "Day_"), num);
-        printf("%s\n", buffer);
-        button = GTK_WIDGET(gtk_builder_get_object(builder, buffer));
-        if (button == NULL) return 1;
-        for (i = 0; i < rowCount; i++) {
-            printf("%s = %s\n", rowName[i], rowValue[i]);
-        }
-        printf("\n");
+
+int import_callback(void *NotUsed, int rowCount, char **rowValue, char **rowName) {
+    return 0;
+}
+
+int import(){
+    save_file = fopen("export.txt", "rt");
+    char line[100];
+    while ( fgets(line, 100, save_file) != NULL) {
+        char * sql;
+        sql = (char *) malloc(100 *sizeof(char));
+        strcpy(sql, "INSERT INTO Account(name, password, type) VALUES(");
+        strncat(sql, line, strlen(line)-1);
+        strncat(sql, ")", 100);
+        printf("\n%s", sql);
+        sqlite3_exec(db, sql, import_callback , 0, &err_msg);
     }
+    fclose(save_file);
+    return 0;
+}
+
+int export_callback(void *NotUsed, int rowCount, char **rowValue, char **rowName) {
+    for (int i = 1; i <= 3; i++) {
+        printf("%s = %s\n", rowName[i], rowValue[i]);
+        if (i != rowCount - 1) fputs("'", save_file);
+        fputs(rowValue[i], save_file);
+        if (i != rowCount - 1) {
+            fputs("'", save_file);
+            fputs(",", save_file);
+        }
+    }
+    fputs("\n", save_file);
+
+
+    printf("\n");
 
     return 0;
+    fclose(save_file);
 }
 
 int main(int argc, char **argv){
 
-    char *err_msg = 0;
+    save_file = fopen("export.txt", "a");
 
-    int rc = sqlite3_open("Projet.database", &db);
+    rc = sqlite3_open("Projet.database", &db);
 
     if (rc != SQLITE_OK) {
         fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
@@ -54,14 +78,16 @@ int main(int argc, char **argv){
     gtk_builder_connect_signals(builder, NULL);
 
     char *sql = "select * from Account";
-    rc = sqlite3_exec(db, sql, callback, 0, &err_msg);
+    sqlite3_exec(db, sql, export_callback, 0, &err_msg);
 
+    import();
+    /*
     gtk_widget_show(window);
 
     gtk_main();
-
+    */
     sqlite3_finalize(res);
     sqlite3_close(db);
-    
+
     return 0;
 }
