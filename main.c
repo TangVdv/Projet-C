349f80283b25e0c50d78ed8530cc99ee710fd8f6
TangVdv <tangrider77120@gmail.com>
2022-01-09 13:04:18 +0100

    Added days and textEdit to the calendar

--- main.c
+++ main.c
@@ -13,6 +13,12 @@ GtkWidget *entry;
 GtkWidget *fileChooser;
 GtkWidget *fichierMenuItem;
 GtkWidget *compteMenuItem;
+GtkWidget *day;
+GtkWidget *btn_calendar_day;
+GtkWidget *grid;
+GtkWidget *textview;
+GtkWidget *saveButton;
+GtkTextBuffer *textbuffer;
 GtkTreeIter iter;
 GtkTreePath *path;
 GtkListStore *ListStore;
@@ -23,6 +29,11 @@ CURL *curl;
 char *err_msg = 0;
 int rc;
 const gchar *btn_calendar_id;
+GtkWidget *btn_id;
+const gchar *btn_value = "";
+
+char tmp[1024]; //Variable for textview
+
 
 char *getEntry_username, *getEntry_password;
 
@@ -34,6 +45,7 @@ char month_name[12][10] = {"Janvier","Février","Mars","Avril","Mai","Juin","Jui
 
 void    on_btn_delete_clicked(GtkWidget *b);
 void    on_btn_overview_clicked(GtkWidget *b);
+void    on_day_clicked (GtkWidget *b);
 
 
 //  BUILD SQL
@@ -178,6 +190,30 @@ void refresh_calendar(){
     gtk_label_set_text(GTK_LABEL(label_year), snum);
     gtk_label_set_text(GTK_LABEL(label_month), month_name[month_id]);
 
+    grid = GTK_WIDGET(gtk_builder_get_object(builder, "grid_day"));
+    char *btn_name;
+    char charValue = '0';
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 7; ++j) {
+            btn_name = (char *) malloc(10 *sizeof(char));
+
+            sprintf(btn_name, "%d", i);
+            strncat(btn_name, "_", 10);
+            charValue = j+'0';
+            strncat(btn_name, &charValue, 10);
+
+            btn_calendar_day = gtk_button_new();
+            gtk_widget_set_hexpand(btn_calendar_day, TRUE);
+            gtk_widget_set_vexpand(btn_calendar_day, TRUE);
+            gtk_widget_set_name(btn_calendar_day, btn_name);
+            g_signal_connect(G_OBJECT(btn_calendar_day), "clicked", G_CALLBACK(on_day_clicked), NULL);
+
+            gtk_grid_attach(GTK_GRID(grid), btn_calendar_day, i,j, 1, 1);
+
+            free(btn_name);
+        }
+    }
+
     gtk_widget_show_all(window);
 }
 
@@ -201,14 +237,76 @@ void main_calendar() {
 
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window_calendar"));
 
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_builder_connect_signals(builder, NULL);
+
     refresh_calendar();
+
+    if (btn_id != NULL) {
+        const char *btn_name = gtk_widget_get_name(btn_id);
+        //day = GTK_WIDGET(gtk_builder_get_object(builder,  btn_name));
+        gtk_button_set_label(GTK_BUTTON(btn_id), "test");
+        //printf("btn_name = %s\n", btn_name);
+        //btn_calendar_day = gtk_button_new_with_label(btn_value);
+        printf("btn_label : %s\n", gtk_widget_get_name(btn_id));
+        printf("btn_value : %s\n", btn_value);
+
+        //gtk_button_set_label(GTK_BUTTON(btn_id), textTest);
+    }
+}
+
+//  CALENDAR\
+
+
+//  /DAY
+
+void show_text_edit(){
+    builder = gtk_builder_new_from_file("textEditW.glade");
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+    gtk_window_set_default_size(GTK_WINDOW(window), 500, 500);
 
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
     gtk_builder_connect_signals(builder, NULL);
+
+    saveButton = GTK_WIDGET(gtk_builder_get_object(builder, "saveButton"));
+    textview = GTK_WIDGET(gtk_builder_get_object(builder, "textview"));
+    //TextEdit
+    textbuffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview));
+    strcpy(tmp, btn_value);
+    gtk_text_buffer_set_text(textbuffer, (const gchar *) tmp, (gint) -1);
+
+    gtk_widget_show_all(window);
 }
 
-//  CALENDAR\
+void on_day_clicked (GtkWidget *b){
+    //printf("Test");
+    gtk_widget_hide(window);
+    btn_id = b;
+    //btn_id = gtk_widget_get_name(GTK_WIDGET(b));
+
+    if ( gtk_button_get_label(GTK_BUTTON(b)) != NULL)
+        btn_value = gtk_button_get_label(GTK_BUTTON(b));
+
+    show_text_edit();
+}
+
+void on_save_button_clicked (GtkButton *b){
+    gtk_widget_hide(window);
+    //printf("Hello");
+    GtkTextIter begin, end;
+
+    gtk_text_buffer_get_iter_at_offset(GTK_TEXT_BUFFER(textbuffer), &begin, (gint) 0);
+    gtk_text_buffer_get_iter_at_offset(GTK_TEXT_BUFFER(textbuffer), &end, (gint) -1);
+
+    btn_value = gtk_text_buffer_get_text(GTK_TEXT_BUFFER(textbuffer), &begin, &end, TRUE);
+
+    refresh_calendar();
+}
+
+//  DAY\
 
 
 
@@ -226,7 +324,7 @@ int main(int argc, char **argv){
     }
     gtk_init(&argc, &argv);
 
-    main_menu();
+    main_calendar();
 
     gtk_main();
 
